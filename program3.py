269714760a6c12e88d2e02983579a3583a181121
keerthew <keerthanakanchukommala@gmail.com>
2025-07-04 09:58:26 +0530

    Add files via upload

---
+++ program3.py
@@ -0,0 +1,16 @@
+# Swapping using a temporary variable
+a = 5
+b = 10
+
+print("Before swapping:")
+print("a =", a)
+print("b =", b)
+
+# Using a temporary variable
+temp = a
+a = b
+b = temp
+
+print("After swapping:")
+print("a =", a)
+print("b =", b)
