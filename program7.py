30d909753575bd0dbe290351810d08f462ae4ca3
keerthew <keerthanakanchukommala@gmail.com>
2025-07-04 09:59:19 +0530

    Add files via upload

---
+++ program7.py
@@ -0,0 +1,8 @@
+def factorial(n):
+    return 1 if n == 0 else n * factorial(n - 1)
+
+def largest_number(lst):
+    return max(lst)
+
+def area_of_rectangle(length, width):
+    return length * width
